e3cb23b5daed8b10217fc07a68e0d5e72849d5e3
rudes <jacobriddle94@gmail.com>
2016-10-01 01:11:38 -0400

    Dynamically render languages

--- server.go
+++ server.go
@@ -31,13 +31,55 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func render(w http.ResponseWriter, content string) {
 	var ctx string
-	t, err := template.ParseFiles(_templateDir+"base.tmpl",
+	tFuncs := template.FuncMap{
+		"printLanguages": printLangs,
+	}
+	t, err := template.New("base.tmpl").Funcs(tFuncs).ParseFiles(_templateDir+"base.tmpl",
 		_templateDir+"header.tmpl", _templateDir+content)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	t.Execute(w, ctx)
+}
+
+func printLangs() template.HTML {
+	var res string
+
+	type language struct {
+		Language, Link string
+	}
+	langs := []language{
+		{"C", "https://en.wikipedia.org/wiki/The_C_Programming_Language"},
+		{"C++", "https://en.wikipedia.org/wiki/C%2B%2B"},
+		{"C#", "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)"},
+		{"Go", "https://golang.org/"},
+		{"Java", "https://en.wikipedia.org/wiki/Java_(programming_language)"},
+		{"Ruby", "https://www.ruby-lang.org/en/"},
+		{"Bash", "https://en.wikipedia.org/wiki/Bash_(Unix_shell)"},
+		{"Powershell", "https://en.wikipedia.org/wiki/PowerShell"},
+		{"Perl", "https://en.wikipedia.org/wiki/Perl"},
+		{"Python", "https://en.wikipedia.org/wiki/Python_(programming_language)"},
+		{"Rust", "https://www.rust-lang.org/en-US/"},
+		{"Haskell", "https://www.haskell.org/"},
+		{"TCL", "https://en.wikipedia.org/wiki/Tcl"},
+		{"VimL", "https://en.wikipedia.org/wiki/Vim_(text_editor)#Vim_script"},
+		{"JavaScript", "https://www.javascript.com/"},
+		{"KSH", "https://en.wikipedia.org/wiki/Korn_shell"},
+		{" ", " "},
+		{"PHP", "https://secure.php.net/"},
+	}
+	for i := 0; i < len(langs); i++ {
+		res += "<tr>"
+		res += "<td><a href='" + langs[i].Link + "'>" + langs[i].Language + "</a></td>"
+		i++
+		res += "<td><a href='" + langs[i].Link + "'>" + langs[i].Language + "</a></td>"
+		i++
+		res += "<td><a href='" + langs[i].Link + "'>" + langs[i].Language + "</a></td>"
+		res += "</tr>"
+	}
+	return template.HTML(res)
+
 }
 
 func staticHandler(w http.ResponseWriter, r *http.Request) {
